e1940682bfe3b799184b3f87f776c8c5edf6ef4d
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-04-06 20:16:45 -0400

    lab 1 completed

--- server/app.py
+++ server/app.py
@@ -21,7 +21,46 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+#Earth View
+@app.route('/earthquakes/<int:id>')
+def earthView(id):
+    body = Earthquake.query.filter(Earthquake.id == id).first() #query filter for my earthquake by id
+        
+    if body is None:
+        return make_response({
+           'message': f'Earthquake {id} not found.'
+        }, 404)
+    
+    return make_response({
+        'id': body.id,
+        'location': body.location,
+        'magnitude': body.magnitude,
+        'year': body.year
+    })
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def mag(magnitude):
+    # Query the database to get all earthquakes with a magnitude greater than or equal to the specified magnitude
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    # Construct a list containing the data for each matched earthquake
+    quakes_data = [{
+        'id': earthquake.id,
+        'location': earthquake.location,
+        'magnitude': earthquake.magnitude,
+        'year': earthquake.year
+    } for earthquake in earthquakes]
+
+    # Construct the JSON response manually
+    response_body = {
+        'count': len(quakes_data),
+        'quakes': quakes_data
+    }
+
+    # Return the response
+    return make_response(response_body)
+
 
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,3 +7,18 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+# model: a blueprint of a class that represents the structure of the data you want to store and organize your data
+
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = "earthquakes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float)
+    location = db.Column(db.String)
+    year = db.Column(db.Integer)
+
+#repr helps to return a string representation of the object.
+
+def __repr__(self):
+    return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
+
